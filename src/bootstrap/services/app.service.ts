1784b3cf3a3368c166185bfe1adfb399457ef27f
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-07-04 13:03:03 -0400

    test with s3

--- src/bootstrap/services/app.service.ts
+++ src/bootstrap/services/app.service.ts
@@ -1,21 +1,25 @@
 import { DB_CONFIG } from '../interfaces/dbConfig.interface'
+import yenv from 'yenv'
+
+const env = yenv('.env')
 
 export class AppService {
-  static get PORT(): number {
-    return +process.env.PORT || 3000
-  }
+   static get PORT(): number {
+      return +env.PORT || 3000
+   }
 
-  static get DBConfig(): DB_CONFIG {
-    return {
-      host: process.env.DB_HOST || 'localhost',
-      port: +process.env.DB_PORT || 3308,
-		//entities: [process.env.DB_ENTITIES || 'src/**/*.entity.ts'],
-      entities: [process.env.DB_ENTITIES || 'dist/**/*.entity.js'],
-      username: process.env.DB_USER || 'adminUser',
-      password: process.env.DB_PASS || '12345678',
-      database: process.env.DB_NAME || 'bddcursonode',
-      synchronize: process.env.DB_SYNC === 'true' ? true : false,
-      logging: process.env.DB_LOGG === 'true' ? true : false,
-    }
-  }
+   static get DBConfig(): DB_CONFIG {
+      const pass = env.DB_PASS.toString();
+      return {
+         host: env.DB_HOST || 'localhost',
+         port: +env.DB_PORT || 3308,
+         //entities: [process.env.DB_ENTITIES || 'src/**/*.entity.ts'],
+         entities: [env.DB_ENTITIES || 'dist/**/*.entity.js'],
+         username: env.DB_USER || 'adminUser',
+         password: pass || '12345',
+         database: env.DB_NAME || 'bddcursonode',
+         synchronize: env.DB_SYNC || false,
+         logging: env.DB_LOGG || false,
+      }
+   }
 }
