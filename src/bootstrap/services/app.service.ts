087573f07dc57ca0f4d8669dbb1824f4fc7555dd
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-07-04 11:05:52 -0400

    credendials of ecr

--- src/bootstrap/services/app.service.ts
+++ src/bootstrap/services/app.service.ts
@@ -12,7 +12,7 @@ export class AppService {
 		//entities: [process.env.DB_ENTITIES || 'src/**/*.entity.ts'],
       entities: [process.env.DB_ENTITIES || 'dist/**/*.entity.js'],
       username: process.env.DB_USER || 'adminUser',
-      password: process.env.DB_PASS || '12345',
+      password: process.env.DB_PASS || '12345678',
       database: process.env.DB_NAME || 'bddcursonode',
       synchronize: process.env.DB_SYNC === 'true' ? true : false,
       logging: process.env.DB_LOGG === 'true' ? true : false,
